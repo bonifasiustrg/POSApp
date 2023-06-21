f2061f0c6b69d7586782dd981ea56f7da7401db3
Bonifasius Tarigan <bonifasius.tambak@gmail.com>
2023-06-21 15:33:20 +0700

    Initial commit - setup project

---
+++ app/src/main/java/com/takasima/posapp/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.takasima.posapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom Color
+val Primary/*Deep_Aquamarine*/ = Color(0xFF3E8765)
+val Secondary/*Eton_Blue*/ = Color(0xFF8DC6AE)
+val Neutral/*Gainsboro*/ = Color(0xFFDDDDDD)
+val May_Green = Color(0xFF4A934A)
+val Orange = Color(0xFFFF8303)
+val Coral_Red = Color(0xFFF44336)
+val Pacific_Blue = Color(0xFF19A7CE)
